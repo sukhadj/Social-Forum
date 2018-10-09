677390559548c0c73f326c6ff72f40cdd20a0174
sukhadj <sukhadj1998@gmail.com>
2018-10-09 19:08:22 +0530

    First Commit

--- forum/routing.py
+++
@@ -1,4 +0,0 @@
-from channels.routing import route
-
-channel_routing=[route('websocket.receive',chat.consumers.ws_echo),
-route('websoket.connect','chat.consumers.ws_add',path=r"^/chat/(?P<room>\w+)$")]
